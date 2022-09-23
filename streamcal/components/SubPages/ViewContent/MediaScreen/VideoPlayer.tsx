f488f902ee72df7714cc9cc35ded00f46d69e0bc
HlDE1 <Natixbox@hotmail.de>
2022-09-23 07:40:08 +0200

    Improved Slider_Preview

--- streamcal/components/SubPages/ViewContent/MediaScreen/VideoPlayer.tsx
+++ streamcal/components/SubPages/ViewContent/MediaScreen/VideoPlayer.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Animated, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, Animated, TouchableOpacity, Image } from "react-native";
 import { Video, AVPlaybackStatus, ResizeMode, PitchCorrectionQuality } from "expo-av";
 
 import React from "react";
@@ -7,6 +7,7 @@ import { MaterialIcons, Octicons } from "@expo/vector-icons";
 import Slider from "@react-native-community/slider";
 import { selectionColor } from "../../../constants/Colors";
 import { Asset } from "expo-asset";
+import { manipulateAsync, SaveFormat } from "expo-image-manipulator";
 
 const videoURL = "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"; //http://192.168.2.121:3005/v1/test2?id=0&season=1&episode=7&dr=video  //"https://eea3-2003-ea-c73b-5f87-41fb-85e8-7931-729f.eu.ngrok.io/v1/test";
 
@@ -27,6 +28,48 @@ function MilisecondsToTimespamp(num: any) {
   else result += sec;
 
   return result;
+}
+
+async function zoomImage(imageURI: any, index: any) {
+  //console.log(WindowSize.Width * 0.14, WindowSize.Width * 0.22);
+  //console.log(sliderPos.current);
+  //const index = 2;
+  if (!imageURI) return;
+
+  const ImagesPerRow = 10;
+  const ImageWidth = 80;
+  const ImageHeight = 45;
+
+  const originX = (index % 10) * ImageWidth;
+  const originY = Math.trunc(index / ImagesPerRow) * ImageHeight;
+
+  //console.log("localUri: ", imageURI.localUri, "uri", imageURI.uri);
+  //console.log("zoomImage: ", imageURI.localUri || imageURI.uri);
+
+  //console.log("originY", originY);
+  //console.log("originX", originX);
+
+  //setImage(imageURI);
+
+  //return;
+  // console.log(originX);
+  const manipResult = await manipulateAsync(
+    imageURI.localUri || imageURI.uri,
+    [
+      {
+        crop: {
+          height: ImageHeight,
+          width: ImageWidth,
+          originX: originX,
+          originY: originY,
+        },
+      },
+    ],
+    { compress: 1, format: SaveFormat.JPEG }
+  );
+  return manipResult.uri;
+  console.log("manipResult:", manipResult.uri);
+  //setImage(manipResult.uri);
 }
 
 const TopButton = ({ isFullscreen, BackButtonOnPress, ScreenButtonOnPress }: any) => (
@@ -157,10 +200,11 @@ const SliderBar: React.FC<ISliderBar> = ({
           {maximumValue ? MilisecondsToTimespamp(maximumValue) : "00:00"}
         </Text>
       </View>
+
       <Slider
         style={{
           width: "102%",
-          height: "50%",
+          height: WindowSize.Width * 0.1, //"55%"
           //flex: 1,
           //backgroundColor: "pink",
           right: WindowSize.Width * 0.01,
@@ -175,6 +219,57 @@ const SliderBar: React.FC<ISliderBar> = ({
         onTouchStart={onTouchStart}
         onSlidingComplete={onSlidingComplete}></Slider>
     </TouchableOpacity>
+  );
+};
+
+interface ISlider_Preview {
+  getSliderValue: any;
+  imageURI: any;
+  status: any;
+}
+const Slider_Preview: React.FC<ISlider_Preview> = ({ getSliderValue, imageURI, status }: any) => {
+  const zoomImageIndex = Math.trunc(getSliderValue / 1000 / 10);
+  //const [getImage, setImage] = React.useState<any>(imageURI || "");
+
+  function pos() {
+    if (!status.durationMillis) return;
+
+    const sliderPercent = getSliderValue / status.durationMillis;
+    const a = WindowSize.Width * sliderPercent - (WindowSize.Width * 0.4) / 2;
+    const leftMargin = WindowSize.Width * 0.03;
+    const rightMargin = WindowSize.Width * 0.57;
+
+    if (leftMargin < a) {
+      if (rightMargin < a) return rightMargin;
+      return a;
+    }
+    if (leftMargin > a) return leftMargin;
+    return a;
+  }
+  //console.log(getSliderValue, status?.durationMillis);
+
+  //console.log("zoomImageIndex", zoomImageIndex);
+  //console.log("Slider_Preview", imageURI);
+  return (
+    <View
+      style={{
+        //opacity: isSliding.current ? 1 : 0,
+        //backgroundColor: "red",
+        width: WindowSize.Width * 0.4,
+        height: WindowSize.Width * 0.25,
+        position: "absolute",
+        top: WindowSize.Width * 0.2,
+        left: pos(),
+      }}>
+      <Image
+        resizeMode="cover"
+        style={{ flex: 1 }}
+        source={{ uri: imageURI }} //getImage
+      ></Image>
+      <Text style={{ color: "white", textAlign: "center" }}>
+        {MilisecondsToTimespamp(getSliderValue)}
+      </Text>
+    </View>
   );
 };
 
@@ -193,19 +288,32 @@ function LoadMediaContent() {
   return isLoaded;
 }
 
-const VideoPlayer = ({ VideoRef }: any) => {
-  const [status, setStatus] = React.useState<any>({});
+let Generated_PreviewImages: Number[] = [];
+
+function SaveGenerated_PreviewImages(zoomImageIndex: any, onAdd: any) {
+  if (Generated_PreviewImages.includes(zoomImageIndex)) return;
+
+  onAdd();
+  Generated_PreviewImages.push(zoomImageIndex);
+}
+
+const VideoPlayer = ({ VideoRef, imageURI }: any) => {
+  // UseStates
+  const [getStatus, setStatus] = React.useState<any>({});
   const [isLoaded, setLoaded] = React.useState<any>(true);
   const [isIcons, setIcons] = React.useState(true);
+  const [getCroppedImage, setCroppedImage] = React.useState<any>(null);
 
+  //UseRefs
   const videoLayout = React.useRef<any>(null);
   const isSliding = React.useRef<any>(false);
-
   const Duration = React.useRef<any>(0);
   const IconsOpacity = React.useRef(new Animated.Value(1)).current;
 
   const [getSliderValue, setSliderValue] = React.useState<any>(0);
 
+  const zoomImageIndex = Math.trunc(getSliderValue / 1000 / 10);
+  //console.log("zoomImageIndex:", zoomImageIndex);
   //const isLoaded2 = LoadMediaContent();
   return (
     <View style={{ ...styles.video_container }}>
@@ -223,16 +331,39 @@ const VideoPlayer = ({ VideoRef }: any) => {
         style={{ ...styles.video }}
         resizeMode={ResizeMode.COVER}
         onPlaybackStatusUpdate={(status: any) => {
-          !isSliding.current && setStatus(() => status);
+          setSliderValue(status.positionMillis);
+          !isSliding.current && setStatus(status);
         }}></Video>
 
-      <Middle_Buttons status={status} VideoRef={VideoRef}></Middle_Buttons>
+      <Animated.View
+        style={{
+          position: "absolute",
+          ...styles.video_container,
+          backgroundColor: !getStatus.isPlaying ? "rgba(0,0,0,0.4)" : "",
+        }}></Animated.View>
+
+      {/* <Middle_Buttons status={getStatus} VideoRef={VideoRef}></Middle_Buttons> */}
+      <Slider_Preview
+        status={getStatus}
+        getSliderValue={getSliderValue}
+        imageURI={getCroppedImage}></Slider_Preview>
       <SliderBar
         isFullscreen={false}
         maximumValue={Duration.current}
-        value={getSliderValue}
-        onValueChange={(e: any) => setSliderValue(e)}
-        onSlidingComplete={() => (isSliding.current = false)}
+        value={getStatus.positionMillis}
+        onValueChange={async (e: any) => {
+          setSliderValue(e);
+          //Generated_PreviewImages.push(zoomImageIndex);
+          SaveGenerated_PreviewImages(zoomImageIndex, async () => {
+            //console.log("Add");
+            setCroppedImage(await zoomImage(imageURI, zoomImageIndex));
+          });
+          //setCroppedImage(await zoomImage(imageURI, zoomImageIndex));
+        }}
+        onSlidingComplete={async (e: any) => {
+          await VideoRef?.current.setPositionAsync(e);
+          isSliding.current = false;
+        }}
         onTouchStart={() => (isSliding.current = true)}></SliderBar>
     </View>
   );
--- streamcal/screens/ListScreen.tsx
+++ streamcal/screens/ListScreen.tsx
@@ -12,17 +12,26 @@ const Cover2 = require("../assets/covers/One_Piece.jpg");
 
 export default function ListScreen() {
   const VideoRef = React.useRef<any>(null);
-  const videoURL = "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4";
+  const [getImage, setImage] = React.useState<any>(null);
+
+  const videoURL = "http://192.168.2.121:3005/v1/test2?id=0&season=1&episode=7&dr=video "; //"http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4";
+  const image = Asset.fromURI(
+    "http://192.168.2.121:3005/v1/test2?id=0&season=1&episode=7&dr=sliderSeek"
+  );
 
   React.useEffect(() => {
     (async () => {
+      // TODO: Running both asyncs at the same time
+      await image.downloadAsync();
+      setImage(image);
+
       await VideoRef?.current?.loadAsync({ uri: videoURL });
     })();
   }, []);
 
   return (
     <View style={styles.container}>
-      <VideoPlayer VideoRef={VideoRef}></VideoPlayer>
+      {getImage && <VideoPlayer VideoRef={VideoRef} imageURI={getImage}></VideoPlayer>}
       {/* <FadingEdgesView
         style={{ width: "100%", height: "100%", borderWidth: 1, borderColor: "green" }}
         ParentBackgroundColor={"red"}>
